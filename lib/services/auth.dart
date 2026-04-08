6533d416e9079c22d81c8ed6e8483f4dd4f500ac
Umer Hayat <umerasrar17676@gmail.com>
2026-04-09 01:27:21 +0500

    Implement user registration with email and password for test only
    Added registerWithEmailAndPassword method for user registration.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -58,13 +58,14 @@ class AuthService {
   }
 
   // sign out
-  Future signOut() async {
+ // register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
     try {
-      return await _auth.signOut();
+      UserCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User user = result.user!;
+      return _userFromFirebaseUser();
     } catch (error) {
-      print(error.toString());
+      print(error.());
       return null;
     }
   }
-
-}
